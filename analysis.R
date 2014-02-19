238749b5611c9b64dd471d4d124f09d0363f4c63
Karen Ross <karen.ross@environment.nsw.gov.au>
2014-02-19 12:39:09 +1100

    Changes made to analaysis.R

--- analysis.R
+++ analysis.R
@@ -1,1 +1,9 @@
+
+test
+# MY BIG ANALYSIS
+
+# probably need some data to subset first
+
+# this will be controled by git
+
 data <- read.csv("data/gapminder-FiveYearData.csv", stringsAsFactors=FALSE)
